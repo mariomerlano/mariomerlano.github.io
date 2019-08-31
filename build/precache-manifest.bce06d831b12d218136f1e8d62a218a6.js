ed41bdbdfe238481abeeb5cc9acd071cd7b6b660
Mario Merlano <mario-merlano@hotmail.com>
2019-08-31 00:59:47 -0300

    react build

---
+++ build/precache-manifest.bce06d831b12d218136f1e8d62a218a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e4a0018b595a55b4c20d1493646568f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b35ef4313265b84f1730",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "cc71c4188f1a4b6f2d04",
+    "url": "/static/js/2.7d54ef27.chunk.js"
+  },
+  {
+    "revision": "b35ef4313265b84f1730",
+    "url": "/static/js/main.3f9eb2aa.chunk.js"
+  },
+  {
+    "revision": "171a7e2bc77364524166",
+    "url": "/static/js/runtime~main.ca8aeaac.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
